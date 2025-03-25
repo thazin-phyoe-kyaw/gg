f91a9e4dcac22cc35381c03c1a6e48d7f050ef4a
thazin-phyoe-kyaw <you@example.com>
2025-03-25 23:19:52 +0630

    d

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { Helmet } from 'react-helmet-async';
+import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet-async";
 
 const MetadataPage = () => {
-  const [metadata, setMetadata] = useState({
-    title: '',
-    description: '',
-    image: '',
-  });
+  // const [metadata, setMetadata] = useState({
+  //   title: '',
+  //   description: '',
+  //   image: '',
+  // });
 
-  useEffect(() => {
-    const fetchMetadata = async () => {
-      try {
-        const response = await fetch('http://localhost:3001/clicks/metadata/123456');
-        // if (!response.ok) {
-        //   throw new Error('Network response was not ok');
-        // }
-      
-        // const data = await response.json();
-        const data  ={
-          title: 'Telegaram Image Preview',
-          description: 'Click here to view more infos',
-          imageUrl:
-            'https://png.pngtree.com/png-clipart/20220620/ourmid/pngtree-pink-cute-cat-icon-animal-png-yuri-png-image_5230763.png',
-        };
-        console.log(data)
-        setMetadata({
-          title: data.title,
-          description: data.description,
-          image: data.image,
-        });
-      } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-      }
-    };
+  const metadata = {
+    title: "Telegaram Image hey",
+    description: "Click here to view more infos",
+    imageUrl:
+      "https://png.pngtree.com/png-clipart/20220620/ourmid/pngtree-pink-cute-cat-icon-animal-png-yuri-png-image_5230763.png",
+  };
+  // setMetadata({
+  //   title: data.title,
+  //   description: data.description,
+  //   image: data.image,
+  // });
+  // useEffect(() => {
+  //   const fetchMetadata = async () => {
+  //     try {
+  //       const response = await fetch('http://localhost:3001/clicks/metadata/123456');
+  //       // if (!response.ok) {
+  //       //   throw new Error('Network response was not ok');
+  //       // }
 
-    fetchMetadata();
-  }, []);
+  //       // const data = await response.json();
+  //       const data  ={
+  //       title: 'Telegaram Image Preview',
+  //       description: 'Click here to view more infos',
+  //       imageUrl:
+  //         'https://png.pngtree.com/png-clipart/20220620/ourmid/pngtree-pink-cute-cat-icon-animal-png-yuri-png-image_5230763.png',
+  //     };
+  //       console.log(data)
+  //       setMetadata({
+  //         title: data.title,
+  //         description: data.description,
+  //         image: data.image,
+  //       });
+  //     } catch (error) {
+  //       console.error('There was a problem with the fetch operation:', error);
+  //     }
+  //   };
+
+  //   fetchMetadata();
+  // }, []);
 
   return (
     <>
