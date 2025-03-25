e82c53a84b50a7a7fe0676c7ea7e53bbd4a9ccc5
thazin-phyoe-kyaw <you@example.com>
2025-03-25 23:08:07 +0630

    test

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const MetadataPage = () => {
+  const [metadata, setMetadata] = useState({
+    title: '',
+    description: '',
+    image: '',
+  });
+
+  useEffect(() => {
+    const fetchMetadata = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/clicks/metadata/123456');
+        // if (!response.ok) {
+        //   throw new Error('Network response was not ok');
+        // }
+      
+        const data = await response.json();
+        console.log(data)
+        setMetadata({
+          title: data.title,
+          description: data.description,
+          image: data.image,
+        });
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
+    };
+
+    fetchMetadata();
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+      </Helmet>
+      <div>
+        <h1>{metadata.title}</h1>
+        <p>{metadata.description}</p>
+        <img src={metadata.image} alt={metadata.title} />
+      </div>
+    </>
+  );
+};
+
+export default MetadataPage;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+    <App />
+    </HelmetProvider>
+  </React.StrictMode>
+  
+);
